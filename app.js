339c4084bcfd121171e9cca86f24a57d1e544ef5
Shawn Wilkinson <me@super3.org>
2017-02-14 13:10:37 -0500

    Basic Download

---
+++ app.js
@@ -0,0 +1,16 @@
+var fs = require('fs');
+var request = require('request');
+
+var download = function(uri, filename, callback){
+  request.head(uri, function(err, res, body){
+    console.log('content-type:', res.headers['content-type']);
+    console.log('content-length:', res.headers['content-length']);
+
+    request(uri).pipe(fs.createWriteStream(filename)).on('close', callback);
+  });
+};
+
+download('https://www.google.com/images/srpr/logo3w.png', 'google.png',
+function(){
+  console.log('done');
+});
