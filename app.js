da6ebf20c2971b608fb8f318b75fc3198a523fbf
Shawn Wilkinson <me@super3.org>
2017-02-14 15:00:04 -0500

    Now Serves Images

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
+'use strict';
+
 var fs = require('fs');
 var request = require('request');
+
+var express = require('express');
+var app = express();
+
+var config = require('./config.js');
 
 var download = function(uri, filename, callback){
   request.head(uri, function(err, res, body){
@@ -10,7 +17,21 @@ var download = function(uri, filename, callback){
   });
 };
 
-download('https://www.google.com/images/srpr/logo3w.png', 'google.png',
-function(){
-  console.log('done');
+app.get('/serve/:url', function(req, res) {
+
+  download(req.params.url, config.cacheDir + 'google.png', function() {
+    console.log('done');
+    res.sendFile(__dirname + '/' + config.cacheDir + 'google.png');
+  });
+
 });
+
+// start the server, if running this script alone
+if (require.main === module) {
+  /* istanbul ignore next */
+  app.listen(3000, function() {
+    console.log('Server listening on port 3000...');
+  });
+}
+
+module.exports = app;
