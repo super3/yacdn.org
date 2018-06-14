38c77254834075ad5b8f8f916c05499c1edc5d22
Shawn Wilkinson <me@super3.org>
2018-06-14 09:24:46 -0400

    Add Koa

--- app.js
+++ app.js
@@ -3,28 +3,34 @@
 const fs = require('fs');
 const url = require('url');
 const path = require('path');
+const util = require('util');
 const request = require('request');
-const express = require('express');
+const Koa = require('koa');
+const Router = require('koa-router');
+const send = require('koa-send');
 
-const app = express();
+const app = new Koa();
+const router = new Router();
 const config = require('./config.js');
 
-const download = function (uri, filename, callback) {
+const download = util.promisify((uri, filename, callback) => {
 	request.head(uri, (err, res) => {
 		if (err) console.log(err);
 		console.log('content-type:', res.headers['content-type']);
 		console.log('content-length:', res.headers['content-length']);
 		request(uri).pipe(fs.createWriteStream(filename)).on('close', callback);
 	});
-};
-
-app.get('/serve/:url', (req, res) => {
-	const filename = path.basename(url.parse(req.params.url).pathname);
-	download(req.params.url, config.cacheDir + filename, () => {
-		console.log('done');
-		res.sendFile(path.join(__dirname, config.cacheDir, filename));
-	});
 });
+
+router.get('/serve/:url', async ctx => {
+	const filename = path.basename(url.parse(ctx.params.url).pathname);
+
+	await download(ctx.params.url, config.cacheDir + filename);
+	await send(ctx, path.join(config.cacheDir, filename));
+	console.log('done');
+});
+
+app.use(router.routes());
 
 // Start the server, if running this script alone
 if (require.main === module) {
