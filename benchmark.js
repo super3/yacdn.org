0f022c66bb445e406addb778ebd6833b4e24a0ea
Monty Anderson <work@montyanderson.net>
2019-03-04 15:54:53 +0000

    benchmark /serve + percentage increase

--- benchmark.js
+++ benchmark.js
@@ -18,7 +18,7 @@ async function getTimes(url, limit) {
 
 function analyseTimes(times) {
 	return {
-		average: times.reduce((a, b) => a + b) / times.length,
+		average: Math.round(times.reduce((a, b) => a + b) / times.length),
 		min: Math.min(...times),
 		max: Math.max(...times)
 	};
@@ -26,20 +26,35 @@ function analyseTimes(times) {
 
 (async () => {
 
-	const yacdn = 'https://yacdn.org/proxy';
+	const yacdn = process.argv.length > 2 ? process.argv[2] : 'https://yacdn.org';
 	const url = 'http://meowbaari.com/wp-content/uploads/2016/06/1464933654_cat_sleep.png';
-	const hits = 10;
+	const hits = 15;
 
-	const nativeTimes = await getTimes(url, hits);
+	const nativeUrl = url;
+	const nativeTimes = await getTimes(nativeUrl, hits);
 	const native = analyseTimes(nativeTimes);
 
-	console.log(`${url}`);
+	console.log(nativeUrl);
 	console.log(`Took ${native.average}ms on average (min: ${native.min}ms, max: ${native.max}ms)\n`);
 
-	const proxyTimes = await getTimes(url, hits);
+	const proxyUrl = `${yacdn}/proxy/${url}`;
+	const proxyTimes = await getTimes(proxyUrl, hits);
 	const proxy = analyseTimes(proxyTimes);
 
-	console.log(`${yacdn}/${url}`);
+	console.log(proxyUrl);
 	console.log(`Took ${proxy.average}ms on average (min: ${proxy.min}ms, max: ${proxy.max}ms)\n`);
+
+	const serveUrl = `${yacdn}/serve/${url}`;
+	const serveTimes = await getTimes(serveUrl, hits);
+	const serve = analyseTimes(serveTimes);
+
+	console.log(serveUrl);
+	console.log(`Took ${serve.average}ms on average (min: ${serve.min}ms, max: ${serve.max}ms)\n`);
+
+	const proxySpeedIncrease = 100 * (1 - (proxy.average / native.average));
+	console.log(`yacdn proxy is ${proxySpeedIncrease.toFixed(2)}% ${proxySpeedIncrease < 0 ? 'slower' : 'faster'}`);
+
+	const serveSpeedIncrease = 100 * (1 - (serve.average / native.average));
+	console.log(`yacdn serve is ${serveSpeedIncrease.toFixed(2)}% ${serveSpeedIncrease < 0 ? 'slower' : 'faster'}`);
 
 })();
