03c5323524d6c1ec2346c7ef2bd78502f5bf7d16
Monty Anderson <work@montyanderson.net>
2019-03-23 17:15:49 +0000

    ignore query string params on serve

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ app.use(async (ctx, next) => {
 
 	const n = await redis.incr('cdnhits');
 
-	const url = `${ctx.path.slice(servePath.length)}?${ctx.querystring}`;
+	const url = `${ctx.path.slice(servePath.length)}?${route === 'proxy' ? ctx.querystring : ''}`;
 
 	debug(`serve#${n} url: ${url}`);
 	debug(`serve#${n} referer: ${ctx.request.headers.referer}`);
