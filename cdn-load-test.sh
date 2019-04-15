684fb81e705e8a9577053fcf8a8d83f7a9e1f84c
Stefan Benten <mail@stefan-benten.de>
2019-04-15 22:51:57 +0200

    Simple Load Testing Script
    This bash script creates load on the CDN by iterating X times over the list of FILESIZES by downloading them.
    You can specify either Proxy or Serve Mode

---
+++ cdn-load-test.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# Usage: ./cdn-test.sh <serve/proxy> <number of iterations>
+
+WORKDIR='/tmp'
+FILESIZES='1MB,10MB,100MB,1GB'
+#Available: 1MB,10MB,100MB,1GB,10GB,50GB,100GB,1000GB
+
+for i in `seq 1 $2`;
+do
+        for SIZE in $(echo $FILESIZES | sed "s/,/ /g");
+        do
+                echo "Downloading $FILE"
+                wget https://yacdn.org/$1/http://speedtest.tele2.net/$SIZE.zip -O $WORKDIR/file.bin
+                rm $WORKDIR/file.bin
+        done
+done
