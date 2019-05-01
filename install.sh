2a0dd10e2235e871e263d84756a41653e428cdc3
Monty Anderson <work@montyanderson.net>
2019-05-01 14:18:43 +0100

    letsencrypt install script

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
+# redis
 apt-get install build-essential -y
 cd ~
 wget http://download.redis.io/releases/redis-5.0.4.tar.gz
@@ -12,16 +13,32 @@ REDIS_PORT=1234 \
  		 REDIS_EXECUTABLE=`command -v redis-server` ./utils/install_server.sh
 ./utils/install_server.sh
 cd ../
+# node
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 nvm install node
+cd /usr/local/bin
+ln -s $HOME/.nvm/versions/node/*/bin/node
+ln -s $HOME/.nvm/versions/node/*/bin/npm
 npm install -g pm2
+ln -s $HOME/.nvm/versions/node/*/bin/pm2
+# yacdn
+cd $HOME/yacdn.org
 apt-get install -y git
 git clone https://github.com/ovsoinc/yacdn.org
-cd yacdn.org
 pm2 start process.json
 pm2 save
 pm2 startup
+# nginx
 apt-get install nginx -y
+curl -o - https://raw.githubusercontent.com/ovsoinc/yacdn.org/master/nginx.conf > /etc/nginx/sites-available/default
+# letsencrypt
+apt-get update
+apt-get install software-properties-common -y
+add-apt-repository universe -y
+add-apt-repository ppa:certbot/certbot -y
+apt-get update -y
+apt-get install certbot python-certbot-nginx -y
+certbot --nginx
